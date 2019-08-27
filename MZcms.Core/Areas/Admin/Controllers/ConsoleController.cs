f73414771d1358870641657e6afb3f274a4fc3ce
MiZone <mizonerom@hotmail.com>
2019-08-27 15:00:55 +0800

    fix privilege

---
+++ MZcms.Core/Areas/Admin/Controllers/ConsoleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MZcms.Web.Framework;
+
+namespace MZcms.Core.Areas.Admin.Controllers
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class ConsoleController : Controller
+    {
+        [HttpGet("Navigation")]
+        public ActionResult<object> Navigation()
+        {
+            var navs = PrivilegeHelper.AdminPrivilegesDefault.Privilege;
+            return Json(new { router = navs });
+        }
+    }
+}
